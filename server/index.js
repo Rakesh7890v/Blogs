8b05e38f0988c96f0f3bcee618f3f9b52081ac45
Rakesh7890v <rakeshrishi7890@gmail.com>
2024-04-21 19:31:48 +0530

    third

--- server/index.js
+++ server/index.js
@@ -7,7 +7,13 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
 
 const app = express()
-app.use(cors());
+app.use(cors(
+    {
+        "origin": ["https://yourblogging.vercel.app/"],
+        "methods": ["POST","GET"],
+        "credentials": true
+    }
+));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(express.json());
