6898bd8d40a6503808861a40f4749b871a02e4fe
Rakesh <147958244+Rakesh7890v@users.noreply.github.com>
2024-04-21 20:10:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,10 +8,7 @@ const bodyParser = require('body-parser');
 
 const app = express()
 
-app.use(cors({
-  origin: 'https://yourblogging.vercel.app',
-  credentials: true
-}));
+app.use(cors());
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
