f9832b1abdc431feacf6e50ff543f67b8b3abb5e
Rakesh7890v <rakeshrishi7890@gmail.com>
2024-04-26 20:20:22 +0530

    update

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ app.use(cors(
         "credentials": true
     }
 ));
+
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(express.json());
